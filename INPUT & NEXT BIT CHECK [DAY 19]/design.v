a5847118862204833cda6cadcb7218dbdc7f5ea9
SUDIPTA DEY <126319513+at-sudipta@users.noreply.github.com>
2023-04-16 20:46:39 +0530

    Add files via upload

---
+++ INPUT & NEXT BIT CHECK [DAY 19]/design.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.04.2023 19:45:20
+// Design Name: 
+// Module Name: design
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module design(
+
+    );
+endmodule
